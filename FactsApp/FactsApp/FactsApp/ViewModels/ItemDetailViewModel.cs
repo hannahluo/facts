52f47a06598114874c62161e3fba55405807b550
krzys <chrisfjski@gmail.com>
2021-01-08 01:12:19 -0500

    Added Android App code to repo

---
+++ FactsApp/FactsApp/FactsApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FactsApp.Models;
+
+namespace FactsApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
