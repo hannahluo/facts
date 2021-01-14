4b87b4f51c21bf12d0564fe5c1174a057fc7d8b1
krzys <chrisfjski@gmail.com>
2021-01-14 02:03:16 -0500

    Updated the default Xamarin code to newer version to fix issues.

---
+++ FactsApp/FactsApp/FactsApp/FactsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FactsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FactsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
