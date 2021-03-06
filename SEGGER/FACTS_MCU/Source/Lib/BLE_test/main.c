17ab517b7819fc6371d0b4fe513cb0a7f3cd4ec2
hannahluo <hannah_luo@hotmail.com>
2021-03-06 00:16:13 -0500

    cleaned up for imu testing

--- SEGGER/FACTS_MCU/Source/Lib/BLE_test/main.c
+++ SEGGER/FACTS_MCU/Source/Lib/BLE_test/main.c
@@ -66,15 +66,6 @@
 //#include "haptic_motors.h"
 #include "bno055.h"
 
-#define MAX_TEST_DATA_BYTES     (15U)                /**< max number of test bytes to be used for tx and rx. */
-#define UART_TX_BUF_SIZE 256                         /**< UART TX buffer size. */
-#define UART_RX_BUF_SIZE 256                         /**< UART RX buffer size. */
-
-#define DBG1_GPIO_PIN 8 
-#define DBG2_GPIO_PIN 9 
-#define STATUS1_PIN 11
-#define STATUS2_PIN 12
-
 #define ELSA_I2C_IMUADDR 0x29
 #define ANNA_I2C_IMUADDR 0x28
 #define ELSA_I2C_MUXADDR 0x71
@@ -114,86 +105,44 @@ int main(void)
     NRF_LOG_INFO("\r\nStart.");
     NRF_LOG_FLUSH();
 
-/*
-    nrf_gpio_cfg_input(STATUS1_PIN, GPIO_PIN_CNF_PULL_Disabled);
-    nrf_gpio_pin_clear(STATUS1_PIN);
-    nrf_gpio_cfg_input(STATUS2_PIN, GPIO_PIN_CNF_PULL_Disabled);
-    nrf_gpio_pin_clear(STATUS2_PIN);
-
-    // NRF_LOG_INFO("\r\nGPIO Setup?");
-    // NRF_LOG_FLUSH();
-    nrf_gpio_cfg_output(DBG1_GPIO_PIN);
-    nrf_gpio_pin_clear(DBG1_GPIO_PIN);
-    nrf_gpio_pin_write(DBG1_GPIO_PIN, 0);
-    nrf_gpio_cfg_output(DBG2_GPIO_PIN);
-    nrf_gpio_pin_clear(DBG2_GPIO_PIN);
-    nrf_gpio_pin_write(DBG2_GPIO_PIN, 0);
-*/
     NRF_LOG_INFO("\r\nInitializing");
     NRF_LOG_FLUSH();
     i2c_init(&i2c_drv);
     while (nrf_drv_twi_is_busy(&i2c_drv)) {};
-    // bool res1 = bno055_init(&elsa_imu);
-    // bool res2 = bno055_init(&anna_imu);
+    bno055_init(&elsa_imu);
+    bno055_init(&anna_imu);
 
-    tca9548a_init(&elsa_mux, 0x5A, &i2c_drv);
-    // tca9548a_init(&anna_mux, ANNA_I2C_MUXADDR, &i2c_drv);
+    /* tca9548a_init(&elsa_mux, ELSA_I2C_MUXADDR, &i2c_drv);
+    tca9548a_init(&anna_mux, ANNA_I2C_MUXADDR, &i2c_drv);
 
     NRF_LOG_INFO("\r\nMotor Setup");
     NRF_LOG_FLUSH();
-    nrf_delay_ms(2500); // give us some time
     drv2605l_init(&elsa_m1, HAPTIC_MOTOR_CH0, &elsa_mux);
-    // put into init -> lib 6 for lra
     drv2605l_library(&elsa_m1, 6);
-    drv2605l_mode(&elsa_m1, 0x07); 
-              uint8_t stat = 0;
-          drv2605l_read(&elsa_m1, 0x01, &stat);
-          NRF_LOG_INFO("MODE: %d", stat);
-                    drv2605l_read(&elsa_m1, 0x00, &stat);
-          NRF_LOG_INFO("STAT: %d", stat);
-    // drv2605l_init(&anna_m1, HAPTIC_MOTOR_CH0, &anna_mux);
+    drv2605l_init(&anna_m1, HAPTIC_MOTOR_CH0, &anna_mux);
+    drv2605l_library(&elsa_m1, 6); */
 
-    // struct bno055_accel_t elsa_data;
-    // struct bno055_accel_t anna_data;
+    struct bno055_accel_t elsa_data;
+    struct bno055_accel_t anna_data;
 
     NRF_LOG_INFO("Entering Loop");
     NRF_LOG_FLUSH();
     while (true)
     {
-  /*
-        nrf_gpio_pin_write(DBG1_GPIO_PIN, 1);
-        nrf_gpio_pin_write(DBG2_GPIO_PIN, 1);
-
-        uint32_t status1 = nrf_gpio_pin_read(STATUS1_PIN);
-        NRF_LOG_INFO("stat1: %d", status1);
-        uint32_t status2 = nrf_gpio_pin_read(STATUS2_PIN);
-        NRF_LOG_INFO("stat2: %d", status2);*/
-
-        drv2605l_waveform(&elsa_m1, 0, 47);
+        /* drv2605l_waveform(&elsa_m1, 0, 47);
         drv2605l_waveform(&elsa_m1, 1, 0);
 
         NRF_LOG_INFO("Motor Run");
         NRF_LOG_FLUSH();
         drv2605l_go(&elsa_m1);
-        int i = 0;
-        while (i < 50) {
-          uint8_t stat = 0;
-          //drv2605l_read(&elsa_m1, GO_REG, &stat);
-          //NRF_LOG_INFO("GO BIT: %d", stat);
-          //NRF_LOG_FLUSH();
-          nrf_delay_ms(100);
-          i += 1;
-        };
-        // drv2605l_stop(&elsa_m1);
+        drv2605l_stop(&elsa_m1); */
 
-        //nrf_gpio_pin_write(DBG1_GPIO_PIN, res1);
-        //nrf_gpio_pin_write(DBG2_GPIO_PIN, res2);
+        bool res1 = bno055_read_accel_xyz(&elsa_data);
+        bool res2 = bno055_read_accel_xyz(&anna_data);
+        // NRF_LOG_INFO("elsa accel x: %d y: %d z: %d", elsa_data.x, elsa_data.y, elsa_data.z);
+        // NRF_LOG_INFO("anna accel x: %d y: %d z: %d", anna_data.x, anna_data.y, anna_data.z);
 
-        //bool res1 = bno055_read_accel_xyz(&elsa_data);
-        //bool res2 = bno055_read_accel_xyz(&anna_data);
-        //NRF_LOG_INFO("elsa accel x: %d y: %d z: %d", elsa_data.x, elsa_data.y, elsa_data.z);
-
-        nrf_delay_ms(7500);
+        nrf_delay_ms(5000);
     }
 }
 
