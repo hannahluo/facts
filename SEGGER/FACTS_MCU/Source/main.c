af5cfc37bf858f6fd0090854e5bfa215b64d48bd
Jack Zhu <jack.jz.zhu@gmail.com>
2021-03-17 01:07:35 -0400

    Initial implementation of FACTS main app

--- SEGGER/FACTS_MCU/Source/main.c
+++ SEGGER/FACTS_MCU/Source/main.c
@@ -1,34 +1,403 @@
-/*********************************************************************
-*                    SEGGER Microcontroller GmbH                     *
-*                        The Embedded Experts                        *
-**********************************************************************
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 
--------------------------- END-OF-HEADER -----------------------------
+#include "nordic_common.h"
+#include "nrf.h"
+#include "app_error.h"
+#include "app_timer.h"
+#include "fds.h"
+#include "sensorsim.h"
+#include "nrf_pwr_mgmt.h"
+#include "nrf_delay.h"
 
-File    : main.c
-Purpose : Generic application start
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
-*/
+#include "bluetooth.h"
+#include "angle_calculation.h"
+#include "i2c.h"
+#include "bno055.h"
+#include "imu.h"
+#include "tca9548a.h"
+#include "drv2605l.h"
+#include "haptic_motors.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+#define DRV_I2C_ADDR     0x5A
+#define ELSA_I2C_IMUADDR 0x29
+#define ANNA_I2C_IMUADDR 0x28
+#define ELSA_I2C_MUXADDR 0x71
+#define ANNA_I2C_MUXADDR 0x70
 
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
-int main(void) {
-  int i;
+#define TCA_SELECT_REG   0
+#define TCA_SELECT_SIZE  1
 
-  for (i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-  }
-  do {
-    i++;
-  } while (1);
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
+
+/* TWI instance. */
+static  nrf_drv_twi_t i2c_drv = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+static struct bno055_t elsa_imu;
+static struct bno055_t anna_imu;
+
+static tca9548a_t elsa_mux; 
+static drv2605l_t elsa_motor;
+
+static tca9548a_t anna_mux;
+static drv2605l_t anna_motor;
+
+static uint8_t HAPTIC_MOTOR_CH0 = 1 << 0;
+static uint8_t HAPTIC_MOTOR_CH1 = 1 << 1;
+static uint8_t HAPTIC_MOTOR_CH2 = 1 << 2;
+
+static uint8_t start_cal = 0;
+static joint_axis_t calfAxis = {.x=0,.y=0,.z=0};
+static joint_axis_t thighAxis = {.x=0,.y=0,.z=0};
+static uint8_t calfAxisUpdated = 0;
+static uint8_t thighAxisUpdated = 0;
+static limits_t angleLim = {.minLimit = 0, .maxLimit = 100};
+
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+    
 }
 
-/*************************** End of file ****************************/
+static void timers_init(void)
+{
+    // Initialize timer module.
+    ret_code_t err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+
+    // Create timers.
+    /*err_code = app_timer_create(&m_double_timer_id, APP_TIMER_MODE_REPEATED, &double_timer_evt_handler);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = app_timer_create(&m_uint_timer_id, APP_TIMER_MODE_REPEATED, &uint_timer_evt_handler);
+    APP_ERROR_CHECK(err_code);*/
+}
+
+static void power_management_init(void)
+{
+    ret_code_t err_code;
+    err_code = nrf_pwr_mgmt_init();
+    APP_ERROR_CHECK(err_code);
+}
+
+void calf_joint_axis_handler(void const * data, uint8_t size)
+{
+    // print out data
+    joint_axis_t* jointAxis = (joint_axis_t*)(data);
+}
+
+void thigh_joint_axis_handler(void const * data, uint8_t size)
+{
+    // print out data
+    joint_axis_t* jointAxis = (joint_axis_t*)(data);
+}
+
+void init_cal_handler(void const * data, uint8_t size)
+{
+    bool isCal = *(bool*)(data);
+    if(isCal) {
+        NRF_LOG_DEBUG("init_cal_handler: init calibration on");
+    } else {
+        NRF_LOG_DEBUG("init_cal_handler: init calibration off");
+    }
+}
+
+void limits_handler(void const* data, uint8_t size)
+{
+    limits_t* limits = (limits_t*)data;
+}
+
+// Initialize ble module
+void ble_module_init()
+{ 
+    NRF_LOG_INFO("\r\n ***BLE Setup Start*** \r\n");
+    bluetooth_init();
+    register_init_cal_handler(&init_cal_handler);
+    register_calf_joint_axis_handler(&calf_joint_axis_handler);
+    register_thigh_joint_axis_handler(&thigh_joint_axis_handler);
+    register_limits_handler(&limits_handler);
+    NRF_LOG_INFO("\r\n ***BLE Setup End*** \r\n");
+}
+
+int8_t motor_init(drv2605l_t* motor, tca9548a_t* mux, uint8_t mux_addr)
+{
+    if(!tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH0, TCA_SELECT_SIZE)) {
+        NRF_LOG_ERROR("Failed to write config to mux ch0");
+        return -1;
+    }
+    if(!drv2605l_init(motor, DRV_I2C_ADDR, mux)) {
+        NRF_LOG_ERROR("Failed to init mux ch0");
+        return -1;
+    }
+    drv2605l_mode(motor, 0);
+    drv2605l_library(motor, 6);
+    drv2605l_waveform(motor, 0, 47);
+    drv2605l_waveform(motor, 1, 0);
+    if(!tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH1, TCA_SELECT_SIZE)) {
+        NRF_LOG_ERROR("Failed to write config to mux ch1");
+        return -1;
+    }
+    if(!drv2605l_init(motor, DRV_I2C_ADDR, mux)) {
+        NRF_LOG_ERROR("Failed to init mux ch1");
+        return -1;
+    }
+    drv2605l_mode(motor, 0);
+    drv2605l_library(motor, 6);
+    drv2605l_waveform(motor, 0, 47);
+    drv2605l_waveform(motor, 1, 0);
+    if(!tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH2, TCA_SELECT_SIZE)) {
+        NRF_LOG_ERROR("Failed to write config to mux ch2");
+        return -1;
+    }
+    if(!drv2605l_init(motor, DRV_I2C_ADDR, mux)) {
+        NRF_LOG_ERROR("Failed to init mux ch2");
+        return -1;
+    }
+    drv2605l_mode(motor, 0);
+    drv2605l_library(motor, 6);
+    drv2605l_waveform(motor, 0, 47);
+    drv2605l_waveform(motor, 1, 0);
+
+    return 0;
+}
+
+// Initialize haptic
+int8_t haptic_module_init()
+{
+    NRF_LOG_INFO("\r\n ***Motor Setup Start*** \r\n");
+    if(motor_init(&elsa_motor, &elsa_mux, ELSA_I2C_MUXADDR) < 0) {
+        NRF_LOG_ERROR("Failed to initialize elsa(thigh) haptics");
+        return -1;
+    }
+    if(motor_init(&anna_motor, &anna_mux, ANNA_I2C_MUXADDR) < 0) {
+        NRF_LOG_ERROR("Failed to initialize anna(calf) haptics");
+        return -1;
+    }
+    NRF_LOG_INFO("\r\n ***Motor Setup End*** \r\n");
+
+    return 0;
+}
+
+void imu_module_init()
+{
+    NRF_LOG_INFO("\r\n***IMU Setup Start***\r\n");
+    bno055_setup(&elsa_imu, &i2c_drv, ELSA_I2C_IMUADDR);
+    bno055_setup(&anna_imu, &i2c_drv, ANNA_I2C_IMUADDR);
+    nrf_delay_ms(100);
+
+    // remap?
+
+    NRF_LOG_INFO("\r\n***IMU Setup End***\r\n");
+}
+
+// Initialize I2C
+void i2c_module_init()
+{
+    NRF_LOG_INFO("\r\n***I2C Setup Start***\r\n");
+    i2c_init(&i2c_drv);
+    while (nrf_drv_twi_is_busy(&i2c_drv)) {};
+    nrf_delay_ms(1000);
+    NRF_LOG_INFO("\r\n***I2C Setup End***\r\n");
+}
+
+void scale_gyro(struct bno055_gyro_t* bno055Gyro, raw_gyro_t* gyro)
+{
+    /* Convert the raw gyro xyz to rps*/
+    gyro->x = (double)(bno055Gyro->x / BNO055_GYRO_DIV_RPS);
+    gyro->y = (double)(bno055Gyro->y / BNO055_GYRO_DIV_RPS);
+    gyro->z = (double)(bno055Gyro->z / BNO055_GYRO_DIV_RPS);
+}
+
+// Calibration
+void calibrate_imu()
+{
+    bno055_get_calibration_status(&i2c_drv, ELSA_I2C_IMUADDR);
+    bno055_get_calibration_status(&i2c_drv, ANNA_I2C_IMUADDR);
+} 
+
+// Calibration
+int8_t calibrate_facts()
+{
+    struct bno055_gyro_t bno055GyroThigh = {.x=0,.y=0,.z=0};
+    struct bno055_gyro_t bno055GyroCalf = {.x=0,.y=0,.z=0};
+
+    raw_gyro_t gyroCalf = {.x = 0,.y=0,.z=0};
+    raw_gyro_t gyroThigh = {.x=0,.y=0,.z=0};
+
+    // set to gyro mode
+    // set units and setup
+
+    // Wait for app to start FACTS cal sequence
+    while(start_cal){};
+
+    // Wait for joint axis to be recvd
+    while(!calfAxisUpdated || !thighAxisUpdated) {
+        // Read
+        if(!bno055_read_gyro(&bno055GyroThigh, &i2c_drv, ELSA_I2C_IMUADDR)) {
+            NRF_LOG_ERROR("Failed to read raw gyro from elsa(thigh)");
+            return -1;
+        }
+        if(!bno055_read_gyro(&bno055GyroCalf, &i2c_drv, ANNA_I2C_IMUADDR)) {
+            NRF_LOG_ERROR("Failed to read raw gyro from anna(calf)");
+            return -1;
+        }
+        
+        // Scale to double
+        scale_gyro(&bno055GyroThigh, &gyroThigh);
+        scale_gyro(&bno055GyroCalf, &gyroCalf);
+
+        // Write to BLE module
+        send_raw_gyro_calf(&gyroCalf);
+        send_raw_gyro_thigh(&gyroThigh);
+
+        // Wait for bus to clear
+        nrf_delay_ms(100);
+    }
+
+    return 0;
+}
+
+void conv_quat_double(struct bno055_quaternion_t* bno055Quat, quat_t* quat)
+{
+    const double scale = (1.0 / (1 << 14));
+    quat->w = (double)bno055Quat->w * scale;
+    quat->x = (double)bno055Quat->x * scale;
+    quat->y = (double)bno055Quat->y * scale;
+    quat->z = (double)bno055Quat->z * scale;
+}
+
+void turn_on_motors(drv2605l_t* motor, uint8_t mux_addr)
+{
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH0, TCA_SELECT_SIZE);
+    drv2605l_go(motor);
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH1, TCA_SELECT_SIZE);
+    drv2605l_go(motor);
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH2, TCA_SELECT_SIZE);
+    drv2605l_go(motor);
+}
+
+void turn_off_motors(drv2605l_t* motor, uint8_t mux_addr)
+{
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH0, TCA_SELECT_SIZE);
+    drv2605l_stop(motor);
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH1, TCA_SELECT_SIZE);
+    drv2605l_stop(motor);
+    tca9548a_write(&i2c_drv, mux_addr, TCA_SELECT_REG, &HAPTIC_MOTOR_CH2, TCA_SELECT_SIZE);
+    drv2605l_stop(motor);
+}
+
+// Calculate
+int8_t get_facts()
+{
+    vector_t thighAxisVec = {.x=thighAxis.x, .y=thighAxis.y, .z=thighAxis.z};
+    vector_t calfAxisVec = {.x=calfAxis.x, .y=calfAxis.y, .z=calfAxis.z};
+    struct bno055_quaternion_t bno055QuatCalf = {.w=0, .x=0, .y=0, .z=0};
+    struct bno055_quaternion_t bno055QuatThigh = {.w=0, .x=0, .y=0, .z=0};
+    quat_t quatCalf = {.w=0,.x=0,.y=0,.z=0};
+    quat_t quatThigh = {.w=0,.x=0,.y=0,.z=0};
+    uint8_t motorsRunning = 0;
+
+    update_joint_axes(&calfAxisVec, &thighAxisVec);
+    double angle = 0;
+    while(1) {
+        // Read quaternion
+        if(!bno055_read_quat(&bno055QuatCalf, &i2c_drv, ANNA_I2C_IMUADDR)) {
+            NRF_LOG_ERROR("Failed to read quaternion from anna(calf)");
+            return -1;
+        }
+        if(!bno055_read_quat(&bno055QuatThigh, &i2c_drv, ELSA_I2C_IMUADDR)) {
+            NRF_LOG_ERROR("Failed to read quaternion from elsa(thigh)");
+            return -1;
+        }
+
+        // Convert to double
+        conv_quat_double(&bno055QuatCalf, &quatCalf);
+        conv_quat_double(&bno055QuatThigh, &quatThigh);
+        
+        // Calc Shit
+        angle = calculate_angle(&quatCalf, &quatThigh);
+
+        // Send to BLE
+        send_flexion_angle(&angle);
+
+        // Check against limits
+        if((angle < angleLim.minLimit || angle > angleLim.maxLimit)) {
+            if(!motorsRunning) {
+                // Turn on
+                motorsRunning = 1;
+                turn_on_motors(&elsa_motor, ELSA_I2C_MUXADDR);
+                turn_on_motors(&anna_motor, ANNA_I2C_MUXADDR);
+            }
+        } else {
+            if(motorsRunning) {
+                // Turn off
+                motorsRunning = 0;
+                turn_off_motors(&elsa_motor, ELSA_I2C_MUXADDR);
+                turn_off_motors(&anna_motor, ANNA_I2C_MUXADDR);
+            }
+        }
+    }
+
+    return 0;
+}
+
+void cleanup()
+{
+    // IMUs
+    deinit(&elsa_imu);
+    deinit(&anna_imu);
+
+    // motor
+    drv2605l_deinit(&elsa_motor);
+    drv2605l_deinit(&anna_motor);
+
+    // mux
+    tca9548a_deinit(&elsa_mux);
+    tca9548a_deinit(&anna_mux);
+
+    // i2c
+    i2c_deinit(&i2c_drv);
+}
+
+#define FICR_BASE_ADDR         (0x10000000)
+#define DEVICE_ID_0_OFFSET     (0x060)
+#define DEVICE_ID_1_OFFSET     (0x064)
+
+int main()
+{
+    log_init();
+    uint32_t device_id[2] = {0};
+    uint32_t* device_id_ptr = (uint32_t*)(FICR_BASE_ADDR+DEVICE_ID_0_OFFSET);
+    device_id[0] = device_id_ptr[0];
+    device_id[1] = device_id_ptr[1];
+    NRF_LOG_INFO("Starting device id 0x%x%x", device_id[0], device_id[1]);
+
+    // Initialize.
+    timers_init();
+    power_management_init();
+    ble_module_init();
+    i2c_module_init();
+    imu_module_init();
+    haptic_module_init();
+
+    // Calibration
+    calibrate_imu();
+    if(calibrate_facts() < 0) {
+        NRF_LOG_ERROR("Failed to calibrate FACTS properly. Shutting down");
+        return -1;
+    }
+    
+    // Calculation
+    get_facts();
+
+    NRF_LOG_INFO("***Shutting Down Facts***");
+}
