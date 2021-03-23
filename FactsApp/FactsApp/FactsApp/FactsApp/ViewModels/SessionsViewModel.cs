93307738bdbdb52b2a7bf3965b9c2ca919899bde
krzys <chrisfjski@gmail.com>
2021-03-23 18:39:09 -0400

    Added scrollable main page, added delete functionality to the sessions

--- FactsApp/FactsApp/FactsApp/FactsApp/ViewModels/SessionsViewModel.cs
+++ FactsApp/FactsApp/FactsApp/FactsApp/ViewModels/SessionsViewModel.cs
@@ -9,6 +9,11 @@ using Xamarin.Forms;
 
 namespace FactsApp.ViewModels
 {
+    public class DataButton : Button
+    {
+        public string data = "";
+    }
+
     public class SessionsViewModel : BaseViewModel
     {
         private string sessionNameText = "None";
@@ -47,7 +52,7 @@ namespace FactsApp.ViewModels
 
         public SessionsViewModel(StackLayout s)
         {
-            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            Device.StartTimer(TimeSpan.FromSeconds(0.5), () =>
             {
                 UpdateView(s);
                 return true;
@@ -64,20 +69,49 @@ namespace FactsApp.ViewModels
                 var button = new Button()
                 {
                     Text = Path.GetFileNameWithoutExtension(file.Name),
+                    HorizontalOptions = LayoutOptions.Center,
                 };
-                button.Clicked += OnDynamicBtnClicked;
-                s.Children.Add(button);
+                button.Clicked += OpenSession;
+
+                var deleteButton = new DataButton()
+                {
+                    Text = "Delete",
+                    HorizontalOptions = LayoutOptions.Center,
+                };
+                deleteButton.Clicked += DeleteSession;
+
+                deleteButton.data = Path.GetFileNameWithoutExtension(file.Name);
+
+                var horizontalLayout = new StackLayout
+                {
+                    Margin = new Thickness(5),
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        button,
+                        deleteButton,
+                    }
+                };
+
+                s.Children.Add(horizontalLayout);
             }
         }
 
-        private void OnDynamicBtnClicked(object sender, EventArgs e)
+        private void OpenSession(object sender, EventArgs e)
         {
             var button = sender as Button;
             SessionNameText = button.Text;
 
             string data = File.ReadAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), SessionNameText + ".csv"));
 
-            var strNumData = data.Split(',');
+            string[] strNumData = data.Split(',');
+
+            // If the user just clicks without any data saved or some other error occurs, make sure we have some data to interpret as a float or the parse call will fail
+            if (strNumData.Length  < 2)
+            {
+                strNumData = new string[] {"0"};
+            }
             List<ChartEntry> newAngleValues = new List<ChartEntry>();
 
             for (int i=0; i < strNumData.Length; ++i)
@@ -88,6 +122,21 @@ namespace FactsApp.ViewModels
             AngleChart = new LineChart()
             {
                 Entries = newAngleValues,
+                IsAnimated = false,
+                MaxValue = 180,
+                MinValue = 0,
+            };
+        }
+
+        private void DeleteSession(object sender, EventArgs e)
+        {
+            var button = sender as DataButton;
+            SessionNameText = "";
+
+            File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), button.data + ".csv"));
+
+            AngleChart = new LineChart()
+            {
                 IsAnimated = false,
                 MaxValue = 180,
                 MinValue = 0,
